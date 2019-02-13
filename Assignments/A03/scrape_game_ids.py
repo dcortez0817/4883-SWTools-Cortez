db8f18225a3e64affc41f84a845898365dacde23
dcortez0817 <darien.cortez@gmail.com>
2019-02-12 23:10:45 -0600

    game ids

--- Assignments/A03/scrape_game_ids.py
+++ Assignments/A03/scrape_game_ids.py
@@ -1,26 +1,43 @@
+"""
+Course: CMPS 4883
+Assignemt: A03
+Date: 2/05/19
+Github username: dacortez0817
+Repo url: https://github.com/dcortez0817/4883-SWTools-Cortez
+Name: Darien Cortez
+Description: 
+    This program scrapes data from NFL.com 
+
+"""
+
 from beautifulscraper import BeautifulScraper
 from pprint import pprint #pretty print
 import urllib #scrapes url from sites
 import json #allows Javascript notation
 import sys
 
-scraper = BeautifulScraper()
+scraper = BeautifulScraper() #variable for scraping data
 
-for year in range(2009, 2019):
-    for week in range(1, 18):
-
-        if season == 'POST':
-                url= "http://www.nfl.com/schedules/" + str(year) + season
+def season_point(season, year, week = "None"):
+        if season == "/REG":
+            url= "http://www.nfl.com/schedules/" + str(year) + season + str(week)
         else:
-                url= "http://www.nfl.com/schedules/" + str(year) + season + str(week)
-
-        page = scraper.go(url) #takes you to the url
+            url= "http://www.nfl.com/schedules/" + str(year) + season 
+        
+        #takes you to the url
+        page = scraper.go(url) 
 
         divs = page.find_all('div',{'class': 'schedules-list-content'})
-        
+
         for div in divs:
                 gameid = div['data-gameid']
                 print (gameid)
         #sys.exit()
 
-        url2 = "http://www.nfl.com/liveupdate/game-center/%d/%d_gtd.json" % (gameid)
+        #url2 = "http://www.nfl.com/liveupdate/game-center/%d/%d_gtd.json" % (gameid)
+
+for y in range(2009, 2019):
+    for w in range(1, 18):
+        season_point("/REG", y, w)
+    season_point("/POST", y)
+        
