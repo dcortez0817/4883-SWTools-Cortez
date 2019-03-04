0fe308b678efd782b8c4e0b60102d38e5d23e3fe
dcortez0817 <darien.cortez@gmail.com>
2019-03-04 01:51:33 -0600

    assignment 4 stuff

---
+++ Assignments/A04/team_games.php
@@ -0,0 +1,41 @@
+<?php
+
+//contains the config information to allow you to connect to the server
+require 'config.php';
+
+//function to run sql query
+require 'query_function.php';
+
+echo "<pre>";   // so whitespace matters
+echo "<h4>Darien Cortez<br></h4>";
+
+//Top ten people that played for the most teams
+//*************************************************************************************************
+echo "1. Count the number of teams an individual played for<br>";
+$sql = "SELECT id, name, COUNT(DISTINCT(season)) as '# seasons', COUNT(DISTINCT(club)) as '# teams'
+        FROM players
+        GROUP BY id, name
+        ORDER BY COUNT(DISTINCT(club)) DESC
+        LIMIT 10"
+
+//run function for sql query
+$response = runQuery($mysqli, $sql);
+
+if($response['success']){
+    foreach($response['result'] as $row){
+        echo "{$row['id']} {$row['name']} {$row['# seasons']} {$row['# teams']}\n";
+    }
+}
+//*************************************************************************************************
+
+//Top five players with the highest total rushing yards
+//*************************************************************************************************
+echo "2. Players that rushed for the most yards per year<br>";
+$sql = "SELECT players_stats.playerid, players.name, players.season,sum(players_stats.yards) as 'total rushing yards'
+        FROM players_stats
+        INNER JOIN players
+        ON players_stats.playerid=players.id
+        WHERE statid=10 or statid=75 or statid=76
+        GROUP BY players_stats.playerid, players.season
+        ORDER BY sum(players_stats.yards) DESC
+        LIMIT 5"
